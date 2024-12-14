2d8ca16838800f9cfe36754af69380b3a67c5384
JUAN ALCANTARA <2481328@gmail.com>
2024-12-14 07:35:32 -0500

    add hotels page and button functionality

--- scripts/index.js
+++ scripts/index.js
@@ -29,17 +29,51 @@ const initialCards = [
   // }
 ];
 
+const hotelCards = [
+  {
+    name: "Hotel 1",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg"
+  },
+  {
+    name: "Hotel 2",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
+  },
+  {
+    name: "Hotel 3",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
+  },
+  {
+    name: "Hotel 4",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
+  },
+  {
+    name: "Hotel 5",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
+  },
+  {
+    name: "Hotel 6",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
+  }
+  // {
+  //   name: "Mountain house",
+  //   link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
+  // }
+];
+
+const cardsHeader = document.querySelector(".cards__header");
 const profileGoButton = document.querySelector(".profile__go-btn");
 const profileSection = document.querySelector(".profile");
 const pageSection = document.querySelector(".page");
 const cardsBackButton = document.querySelector(".cards__back-btn");
+const nextHotelButton = document.querySelector(".cards__nexthotel-btn");
+const exitAppButton = document.querySelector(".cards__exit-btn");
 
 const cardsSection = document.querySelector(".cards");
 const cardContentContainer = document.querySelector(".cards__pics");
 
-const cardHeartButton = cardContentContainer.querySelector(
-  ".card__footer-heart-btn"
-);
+// const cardHeartButton = cardContentContainer.querySelector(
+//   ".card__footer-heart-btn"
+// );
 
 // const closeModalListener = (event) => {
 //   if (event.target.classList.contains("modal_opened")) {
@@ -117,7 +151,7 @@ function getCardElement(data) {
   // });
 
   cardImage.addEventListener("click", () => {
-    openImageModal(cardElement);
+    // openImageModal(cardElement);
   });
 
   return cardElement;
@@ -126,80 +160,30 @@ function handleLikeButton(cardEl) {
   cardEl.classList.toggle("card__footer-heart-btn-liked");
 }
 
-// function handleDeleteButton(cardEl) {
-//   const cardName = cardEl.alt;
-
-//   const index = initialCards.findIndex((card) => card.name === cardName);
-
-//   initialCards.toSpliced(1, index);
-//   cardEl.remove();
-// }
-
-// function openImageModal(cardEl) {
-//   const cardImage = cardEl.querySelector(".card__image");
-//   const modalCaption = cardEl.querySelector(".card__footer-title");
-
-//   modalFooterCaption.textContent = modalCaption.textContent;
-
-//   modalImage.src = "";
-
-//   openModal(imageZoomModal);
-
-//   modalImage.src = cardImage.src;
-//   modalImage.alt = cardImage.alt;
-// }
-
-// profileEditButton.addEventListener("click", () => {
-// profileNameInput.value = profileUserName.textContent;
-// profileDescriptionInput.value = profileUserDescription.textContent;
-// resetValidation(
-//   editProfileModal,
-//   [profileNameInput, profileDescriptionInput],
-//   settings
-// );
-// openModal(editProfileModal);
-// });
-
-// addPostButton.addEventListener("click", () => {
-//   openModal(addPostModal);
-// });
-
-// profileCloseButton.addEventListener("click", () => {
-//   closeModal(editProfileModal);
-// });
-
-// closePostModalButton.addEventListener("click", () => {
-//   closeModal(addPostModal);
-// });
-
-// closeZoomModalButton.addEventListener("click", () => {
-//   closeModal(imageZoomModal);
-// });
-
-// const closeModalEscapeListener = (event) => {
-//   if (event.key === "Escape") {
-//     const openModals = document.querySelectorAll(".modal_opened");
-//     openModals.forEach(closeModal);
-//   }
-// };
+function displayCards(cardData) {
+  cardData.forEach((item) => {
+    cardContentContainer.append(getCardElement(item));
+  });
+}
 
 profileGoButton.addEventListener("click", () => {
+  cardsHeader.textContent = "Select Bars";
+  cardContentContainer.innerHTML = "";
   profileSection.style = "display: none";
   pageSection.style = "background-color: #EAE7E5";
-
+  const barCards = displayCards(initialCards);
   cardsSection.style.display = "block";
 });
 
-cardsBackButton.addEventListener("click", () => {
+exitAppButton.addEventListener("click", () => {
   profileSection.style.display = "";
   pageSection.style = "background-color: #1e1e1e";
 
   cardsSection.style.display = "none";
 });
 
-// profileForm.addEventListener("submit", handleProfileFormSubmit);
-// addModalForm.addEventListener("submit", handlePostFormSubmit);
-
-initialCards.forEach((item) => {
-  cardContentContainer.append(getCardElement(item));
+nextHotelButton.addEventListener("click", () => {
+  cardsHeader.textContent = "Select Hotels";
+  cardContentContainer.innerHTML = "";
+  displayCards(hotelCards);
 });

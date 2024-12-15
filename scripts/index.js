2bc33a004b4248fc416011c193e8689281f73ac1
JUAN ALCANTARA <2481328@gmail.com>
2024-12-15 12:59:48 -0500

    add constants.js move constants to it

--- scripts/index.js
+++ scripts/index.js
@@ -1,133 +1,21 @@
-const initialCards = [
-  {
-    name: "Golden Gate Bridge",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg"
-  },
-  {
-    name: "Val Thorens",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
-  },
-  {
-    name: "Restaurant terrace",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
-  },
-  {
-    name: "An outdoor cafe",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
-  },
-  {
-    name: "A very long bridge, over the forest and through the trees",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
-  },
-  {
-    name: "Tunnel with morning light",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
-  }
-  // {
-  //   name: "Mountain house",
-  //   link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
-  // }
-];
+import {
+  initialCards,
+  hotelCards,
+  cardsHeader,
+  profileGoButton,
+  profileSection,
+  pageSection,
+  cardsBackButton,
+  selectHotelButton,
+  nextHotelButton,
+  exitAppButton,
+  infoAppButton,
+  footerSection,
+  cardsSection,
+  cardContentContainer
+} from "../utils/constants.js";
 
-const hotelCards = [
-  {
-    name: "Hotel 1",
-    link: "../images/pexels-hotel-1.jpg"
-  },
-  {
-    name: "Hotel 2",
-    link: "../images/pexels-hotel-2.jpg"
-  },
-  {
-    name: "Hotel 3",
-    link: "../images/pexels-hotel-3.jpg"
-  },
-  {
-    name: "Hotel 4",
-    link: "../images/pexels-hotel-4.jpg"
-  },
-  {
-    name: "Hotel 5",
-    link: "../images/pexels-hotel-5.jpg"
-  },
-  {
-    name: "Hotel 6",
-    link: "../images/pexels-hotel-6.jpg"
-  }
-  // {
-  //   name: "Mountain house",
-  //   link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
-  // }
-];
-
-const cardsHeader = document.querySelector(".cards__header");
-const profileGoButton = document.querySelector(".profile__go-btn");
-const profileSection = document.querySelector(".profile");
-const pageSection = document.querySelector(".page");
-const cardsBackButton = document.querySelector(".cards__back-btn");
-const selectHotelButton = document.querySelector(".cards__selecthotel-btn");
-const nextHotelButton = document.querySelector(".cards__nexthotel-btn");
-const exitAppButton = document.querySelector(".cards__exit-btn");
-const infoAppButton = document.querySelector(".cards__info-btn");
-
-const footerSection = document.querySelector(".footer");
-
-const cardsSection = document.querySelector(".cards");
-const cardContentContainer = document.querySelector(".cards__pics");
-
-// const cardHeartButton = cardContentContainer.querySelector(
-//   ".card__footer-heart-btn"
-// );
-
-// const closeModalListener = (event) => {
-//   if (event.target.classList.contains("modal_opened")) {
-//     closeModal(event.target);
-//   }
-// };
-
-// function openModal(modal) {
-//   modal.classList.add("modal_opened");
-//   document.body.addEventListener("click", closeModalListener);
-//   document.addEventListener("keydown", closeModalEscapeListener);
-// }
-
-// function closeModal(modal) {
-//   modal.classList.remove("modal_opened");
-//   document.body.removeEventListener("click", closeModalListener);
-//   document.removeEventListener("keydown", closeModalEscapeListener);
-// }
-
-// function handleProfileFormSubmit(evt) {
-//   evt.preventDefault();
-
-//   profileUserName.textContent = profileNameInput.value;
-//   profileUserDescription.textContent = profileDescriptionInput.value;
-
-//   closeModal(editProfileModal);
-// }
-
-// function handlePostFormSubmit(evt) {
-//   evt.preventDefault();
-
-//   const newCard = {
-//     name: addModalFormCaption.value,
-//     link: addModalFormLink.value
-//   };
-//   if (addModalFormCaption.value !== "" && addModalFormLink.value !== "") {
-//     initialCards.unshift(newCard);
-
-//     cardContentContainer.prepend(getCardElement(newCard));
-
-//     closeModal(addPostModal);
-//   }
-
-//   addModalFormCaption.value = "";
-//   addModalFormLink.value = "";
-
-//   const inputList = Array.from(addPostModal.querySelectorAll(".modal__input"));
-//   const buttonElement = addPostModal.querySelector(".modal__submit-btn");
-//   toggleButtonState(inputList, buttonElement, settings);
-// }
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
 function getCardElement(data) {
   const cardElement = document
@@ -141,6 +29,8 @@ function getCardElement(data) {
 
   cardImage.src = data.link;
   cardImage.alt = data.name;
+
+  const id = uuidv4();
 
   // const cardHeartBtn = cardElement.querySelector(".card__footer-heart-btn");
 
---
+++ utils/constants.js
@@ -0,0 +1,78 @@
+export const initialCards = [
+  {
+    name: "Golden Gate Bridge",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg"
+  },
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
+  }
+  // {
+  //   name: "Mountain house",
+  //   link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
+  // }
+];
+
+export const hotelCards = [
+  {
+    name: "Hotel 1",
+    link: "../images/pexels-hotel-1.jpg"
+  },
+  {
+    name: "Hotel 2",
+    link: "../images/pexels-hotel-2.jpg"
+  },
+  {
+    name: "Hotel 3",
+    link: "../images/pexels-hotel-3.jpg"
+  },
+  {
+    name: "Hotel 4",
+    link: "../images/pexels-hotel-4.jpg"
+  },
+  {
+    name: "Hotel 5",
+    link: "../images/pexels-hotel-5.jpg"
+  },
+  {
+    name: "Hotel 6",
+    link: "../images/pexels-hotel-6.jpg"
+  }
+  // {
+  //   name: "Mountain house",
+  //   link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
+  // }
+];
+
+export const cardsHeader = document.querySelector(".cards__header");
+export const profileGoButton = document.querySelector(".profile__go-btn");
+export const profileSection = document.querySelector(".profile");
+export const pageSection = document.querySelector(".page");
+export const cardsBackButton = document.querySelector(".cards__back-btn");
+export const selectHotelButton = document.querySelector(
+  ".cards__selecthotel-btn"
+);
+export const nextHotelButton = document.querySelector(".cards__nexthotel-btn");
+export const exitAppButton = document.querySelector(".cards__exit-btn");
+export const infoAppButton = document.querySelector(".cards__info-btn");
+
+export const footerSection = document.querySelector(".footer");
+
+export const cardsSection = document.querySelector(".cards");
+export const cardContentContainer = document.querySelector(".cards__pics");
